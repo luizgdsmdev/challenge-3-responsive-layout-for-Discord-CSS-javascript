7fa5c463fcd09afc457806d4511d88b6792391d7
luiz messias <luizgdsmdev@gmail.com>
2025-07-23 23:44:54 -0300

    Adding javascript for motion on text and image elements

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= ((window.innerHeight -120) || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function updateHtmlElement(element, image, text, textDirection, imageDirection){
+    window.addEventListener('scroll', () => {
+        console.log(element);
+        if (isElementInViewport(element)) {
+            text.classList.add(textDirection);
+            image.classList.add(imageDirection);
+        }
+    });
+}
+
+
+
+let secondSectionImg = document.getElementById('second-section-img');
+let secondSectionText = document.getElementById('second-section-text');
+let ThirdSectionImg = document.getElementById('third-section-img');
+let ThirdSectionText = document.getElementById('third-section-text');
+let FourthSectionImg = document.getElementById('fourth-section-img');
+let FourthSectionText = document.getElementById('fourth-section-text');
+let lastSectionImg = document.getElementById('last-section-img');
+let lastSectionText = document.getElementById('last-section-text');
+
+
+
+updateHtmlElement(secondSectionImg, secondSectionImg, secondSectionText, "ShowTextFromLeft", "ShowImageFromLeft");
+updateHtmlElement(ThirdSectionText, ThirdSectionText, ThirdSectionImg, "ShowTextFromRigh", "ShowImageFromRigh");
+updateHtmlElement(FourthSectionImg, FourthSectionImg, FourthSectionText, "ShowTextFromRigh", "ShowImageFromRigh");
+updateHtmlElement(lastSectionText, lastSectionText, lastSectionImg, "ShowTextFromBottom", "ShowImageFromBottom");
+
+
+// if (secondSectionImg) {
+//     window.addEventListener('scroll', () => {
+//         if (isElementInViewport(secondSectionImg)) {
+//             secondSectionText.classList.add("ShowTextFromLeft");
+//             secondSectionImg.classList.add("ShowImageFromLeft");
+//         }
+//     });
+// }
